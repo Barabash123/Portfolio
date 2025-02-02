0b1f09cf59d94f19367c7fe020f2bec6bfc55a7f
PashaDev2 <pasha.yakubovsky@lynksen.com>
2025-02-02 23:38:47 +0200

    +connect to deployed backend

--- src/lib/api/constants.js
+++ src/lib/api/constants.js
@@ -1,6 +1,6 @@
 const constants = {
-	// apiUrl: 'https://taia-cms-thrumming-flower-7284.fly.dev',
-	apiUrl: 'http://localhost:3000'
+	apiUrl: 'https://taia-cms-thrumming-flower-7284.fly.dev'
+	// apiUrl: 'http://localhost:3000'
 };
 
 export { constants };
